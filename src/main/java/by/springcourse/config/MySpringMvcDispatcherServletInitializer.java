c4f1afc778beb534a244958eb382f1c13f6ea87c
Alexey Bobrikov <a.beaverg@gmail.com>
2023-07-27 15:52:27 +0300

    upload lesson 23

--- src/main/java/by/springcourse/config/MySpringMvcDispatcherServletInitializer.java
+++ src/main/java/by/springcourse/config/MySpringMvcDispatcherServletInitializer.java
@@ -1,5 +1,8 @@
 package by.springcourse.config;
 
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 /**
@@ -20,5 +23,16 @@ public class MySpringMvcDispatcherServletInitializer extends AbstractAnnotationC
     @Override
     protected String[] getServletMappings() {
         return new String[] {"/"};
+    }
+
+    @Override
+    public void onStartup(ServletContext aServletContext) throws ServletException {
+        super.onStartup(aServletContext);
+        registerHiddenFieldFilter(aServletContext);
+    }
+
+    private void registerHiddenFieldFilter(ServletContext aContext) {
+        aContext.addFilter("hiddenHttpMethodFilter",
+                new HiddenHttpMethodFilter()).addMappingForUrlPatterns(null ,true, "/*");
     }
 }
